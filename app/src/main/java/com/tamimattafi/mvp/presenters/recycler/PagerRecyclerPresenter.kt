2533d03b3cd460ff7d0a3e74c2dcf49328051668
iToxicDeveloper <tamim.tanki@gmail.com>
2019-11-09 22:13:16 +0700

    Fixes for PagerRecyclerAdapter.

--- app/src/main/java/com/tamimattafi/mvp/presenters/recycler/PagerRecyclerPresenter.kt
+++ app/src/main/java/com/tamimattafi/mvp/presenters/recycler/PagerRecyclerPresenter.kt
@@ -6,7 +6,7 @@ import com.tamimattafi.mvp.presenters.recycler.global.BaseRecyclerPresenter
 abstract class PagerRecyclerPresenter<T, H : Holder, V : ListenerView<H>, R : PagerListRepository<T>>(view: V, repository: R) :
     BaseRecyclerPresenter<T, H, PagerAdapter, V, R>(view, repository) {
 
-    private var page: Int = 1
+    protected var page: Int = 1
 
     override fun loadRepositoryData() {
 
