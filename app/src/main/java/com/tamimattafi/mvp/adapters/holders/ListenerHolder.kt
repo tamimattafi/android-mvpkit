47ebfeaa0acd4415987e7fa3a4d6e2e96e9317f9
iToxicDeveloper <tamim.tanki@gmail.com>
2019-11-10 11:18:58 +0700

    Addition of ListenerHolder.

---
+++ app/src/main/java/com/tamimattafi/mvp/adapters/holders/ListenerHolder.kt
@@ -0,0 +1,19 @@
+package com.tamimattafi.mvp.adapters.holders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.tamimattafi.mvp.MvpBaseContract
+
+open class ListenerHolder(itemView: View) : RecyclerView.ViewHolder(itemView), MvpBaseContract.ListenerHolder {
+
+    protected var onAction: ((action: Int) -> Unit)? = null
+
+    override fun setHolderClickListener(onClick: () -> Unit): MvpBaseContract.ListenerHolder = this.also {
+        itemView.setOnClickListener {
+            onClick.invoke()
+        }
+    }
+
+    override fun setHolderActionListener(onAction: (action: Int) -> Unit): MvpBaseContract.ListenerHolder =
+        this.also { it.onAction = onAction }
+}
--- app/src/main/java/com/tamimattafi/mvp/adapters/holders/empty/UnbindableHolder.kt
+++ app/src/main/java/com/tamimattafi/mvp/adapters/holders/empty/UnbindableHolder.kt
@@ -3,4 +3,4 @@ package com.tamimattafi.mvp.adapters.holders.empty
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class UnbindableHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+open class UnbindableHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
