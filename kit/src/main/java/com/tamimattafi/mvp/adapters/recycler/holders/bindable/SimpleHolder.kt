3527e0e45e088f9db13d4731f5e3be6086c37428
iToxicDeveloper <tamim.tanki@gmail.com>
2020-01-09 14:08:42 +0700

    Creation of SimpleHolder

--- kit/src/main/java/com/tamimattafi/mvp/adapters/recycler/holders/bindable/SimpleHolder.kt
+++ kit/src/main/java/com/tamimattafi/mvp/adapters/recycler/holders/bindable/SimpleHolder.kt
@@ -3,14 +3,14 @@ package com.tamimattafi.mvp.adapters.recycler.holders.bindable
 import android.util.Log
 import android.view.View
 
-open class SimpleHolder(view: View) : RecyclerHolder(view) {
+open class SimpleHolder(view: View) : ListenerHolder(view) {
 
     protected fun invokeClick() {
-        onClick?.invoke() ?: Log.d(this.javaClass.simpleName, "onClick listener for list position $listPosition is null")
+        onClick?.invoke() ?: Log.d(this.javaClass.simpleName, "onClick listener is null")
     }
 
     protected fun invokeAction(action: Int) {
-        onAction?.invoke(action) ?: Log.d(this.javaClass.simpleName, "onAction listener for list position $listPosition is null\n tried to invoke action: $action")
+        onAction?.invoke(action) ?: Log.d(this.javaClass.simpleName, "onAction listener is null\n tried to invoke action: $action")
     }
 
 }
