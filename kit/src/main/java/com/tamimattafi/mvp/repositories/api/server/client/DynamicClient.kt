cab9f06697191d9f0a9715aa4fd2421147b85a43
iToxicDeveloper <tamim.tanki@gmail.com>
2020-01-30 17:09:30 +0700

    Listening capabilities for dynamic client

--- kit/src/main/java/com/tamimattafi/mvp/repositories/api/server/client/DynamicClient.kt
+++ kit/src/main/java/com/tamimattafi/mvp/repositories/api/server/client/DynamicClient.kt
@@ -15,13 +15,28 @@ interface DynamicClient {
     @PUT
     fun put(@Url url: String): Call<Void>
 
+    @PUT
+    fun putListen(@Url url: String, @Body any: Any): Call<ResponseBody>
+
+    @PUT
+    fun putListen(@Url url: String): Call<ResponseBody>
+
     @POST
     fun post(@Url url: String, @Body any: Any): Call<Void>
 
     @POST
     fun post(@Url url: String): Call<Void>
 
+    @POST
+    fun postListen(@Url url: String, @Body any: Any): Call<ResponseBody>
+
+    @POST
+    fun postListen(@Url url: String): Call<ResponseBody>
+
     @DELETE
     fun delete(@Url url: String): Call<Void>
+
+    @DELETE
+    fun deleteListen(@Url url: String): Call<ResponseBody>
 
 }
